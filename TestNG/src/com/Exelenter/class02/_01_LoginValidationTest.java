d2921047dbc7cc965c78ad0a9ca7440712679bee
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-02-05 16:10:19 +0300

    page/test, configs packages added

--- TestNG/src/com/Exelenter/class02/_01_LoginValidationTest.java
+++ TestNG/src/com/Exelenter/class02/_01_LoginValidationTest.java
@@ -3,11 +3,11 @@ package com.Exelenter.class02;
 public class _01_LoginValidationTest {
     /**
      * Todo: We will copy from Selenium Project following folders & packages:
-     *    1. utils package
-     *    2. page package
-     *    3. test package
+     *    1. utils package ok
+     *    2. page package ok
+     *    3. test package ok
      *    4. Selenium JARs.
-     *    5. Drivers folder
+     *    5. Drivers folder ok
      *    6. configs folder
      */
 
