80b909a9b0a0371886c9d3767fedf0204296cd07
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-02-08 20:54:19 +0300

    SoftAssert introduced, test report introduced,

--- TestNG/src/com/Exelenter/class03/_00_Assert_HW.java
+++ TestNG/src/com/Exelenter/class03/_00_Assert_HW.java
@@ -1,15 +1,37 @@
 package com.Exelenter.class03;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import utils.BaseClass;
+
 /**
  *  Task: Hard Assert & Soft Assertion
  *    Hard Assert
- *       1. Go to exelentersdet.com website
+ *       1. Go to z website
  *       2. Get the title of the webpage, change it to false, fail this test step intentionally
  *       3. Login to the website. You should NOT be able to login
  *    Now, comment out Hard Assert, and use soft Assert instead.
  *       4. Using Soft Assert, try to login to the website.
  *    You should be able to login, even if the title verification (previous step) fails.
  */
-public class _00_Assert_HW {
+public class _00_Assert_HW extends BaseClass {
+    @BeforeTest
+    void beforeTest(){
+        setUp();
+    }
+    @AfterTest
+    void afterTest(){
+        tearDown();
+    }
+    @Test
+    void hardAssert(){
+        LoginPage loginPage = new LoginPage();
+        String expectedTitle = "Exelenter Project";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle,expectedTitle,"Exelenter Project is wrong.");
+    }
 
 }
