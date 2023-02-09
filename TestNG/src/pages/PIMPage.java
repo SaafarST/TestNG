95c87772e99ec630a144fa7953e7d5ea72972cde
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-02-09 21:42:24 +0300

    AddEmployeePage and PIMPage are  introduced

---
+++ TestNG/src/pages/PIMPage.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.BaseClass;
+
+public class PIMPage extends BaseClass {
+    @FindBy(id = "menu_pim_viewPimModule")
+    public WebElement PIM;
+    @FindBy(id = "menu_pim_viewEmployeeList")
+    public WebElement employeeList;
+    @FindBy(id = "menu_pim_addEmployee")
+    public WebElement addEmployee;
+
+    public PIMPage() {
+        PageFactory.initElements(driver,this);
+    }
+    public void navigateToAddEmployee(){
+        click(PIM);
+        click(employeeList);
+    }
+}
---
+++ TestNG/src/test/addEmployeeTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utils.BaseClass;
+
+public class addEmployeeTest extends BaseClass {
+    @BeforeMethod
+    void startBrowser() {
+        setUp();
+    }
+    @AfterMethod
+    void quitBrowser() {
+        tearDown();
+    }
+
+}
