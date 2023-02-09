bca60ec3ee269729e1267d3d984ab56f07e6a211
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-02-09 22:03:50 +0300

    AddEmployeeTest tested

--- TestNG/src/test/addEmployeeTest.java
+++ TestNG/src/test/addEmployeeTest.java
@@ -3,7 +3,9 @@ package test;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.AddEmployeePage;
 import pages.LoginPage;
+import pages.PIMPage;
 import utils.BaseClass;
 import utils.ConfigsReader;
 
@@ -16,12 +18,20 @@ public class addEmployeeTest extends BaseClass {
     void quitBrowser() {
         tearDown();
     }
-
-
-
     @Test
     public void addEmployeeTest(){
         var loginPage = new LoginPage();// Happy Path Testing
         loginPage.loginToWebsite(ConfigsReader.getProperties("username"),ConfigsReader.getProperties("password"));
+
+        PIMPage pimPage = new PIMPage();
+        pimPage.navigateToAddEmployee();
+
+        AddEmployeePage addEmployee = new AddEmployeePage();
+        String name = "Eva";
+        String surname = "Brown";
+        sendText(addEmployee.firstName,name);
+        sendText(addEmployee.lastName,surname);
+        sendText(addEmployee.uploadPhoto, ConfigsReader.getProperties("filePath"));
+        click(addEmployee.saveButton);
     }
 }
