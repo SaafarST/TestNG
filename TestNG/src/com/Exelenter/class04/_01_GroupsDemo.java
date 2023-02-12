2f2497a16a2c3a2dd330307ff375cf52c54dd13f
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-02-12 17:32:23 +0300

    Groups test introduced

---
+++ TestNG/src/com/Exelenter/class04/_01_GroupsDemo.java
@@ -0,0 +1,21 @@
+package com.Exelenter.class04;
+
+import org.testng.annotations.Test;
+
+public class _01_GroupsDemo {
+    @Test
+    void test1(){
+        System.out.println("Test 1");}
+    @Test(groups = "smoke")
+    void test2(){
+        System.out.println("Test 2");
+    }
+    @Test(groups = "regression")
+    void test3(){
+        System.out.println("Test 3");
+    }
+    @Test(groups = {"smoke","regression"})
+    void test4(){
+        System.out.println("Test 4");
+    }
+}
