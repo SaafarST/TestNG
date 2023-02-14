e45f95b94a61f795377342ea9be5c3e936829104
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-02-14 18:49:02 +0300

    Data Provider introduced

--- TestNG/src/com/Exelenter/class04/_02_DataProviderDemo.java
+++ TestNG/src/com/Exelenter/class04/_02_DataProviderDemo.java
@@ -1,8 +1,10 @@
 package com.Exelenter.class04;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -37,16 +39,29 @@ public class _02_DataProviderDemo {
 
         };
         System.out.println(user);
-        for (Object o : users) {
-            System.out.println(o);
-        }
-        for (Object[] objects : users2D) {
-            for (Object object : objects) {
-                System.out.print(object);
-            }
-            System.out.println("\n======");
-        }
-
+//        for (Object o : users) {
+//            System.out.println(o);
+//        }
+//        for (Object[] objects : users2D) {
+//            for (Object object : objects) {
+//                System.out.print(" "+object);
+//            }
+//            System.out.println("\n");
+//        }
+        Arrays.stream(users2D).forEach(a -> System.out.println(Arrays.toString(a)));
     }
-
+    // Manipulating DATA using DataProvider
+    @Test(dataProvider = "users")
+    public void printData(String firstName, String lastName,int age){
+        System.out.println(firstName+" "+lastName+" "+age);
+    }
+    @DataProvider
+    public Object[][] users(){
+        Object[][] users = {
+                {"John ","Sam ",29},
+                {"Mich","Lee",14},
+                {"Josh","Lee",24}
+        };
+        return users;
+    }
 }
